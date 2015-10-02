e6b86d6b0bd73dbde59a281c1ae932407183af8a
Jonah Bull <jonahbull@gmail.com>
2015-10-02 12:22:51 -0500

    Add subclass that auto-extends lock expiry

--- redlock/__init__.py
+++ redlock/__init__.py
@@ -1,5 +1,6 @@
 import string
 import random
+import threading
 import time
 from collections import namedtuple
 
@@ -96,4 +97,68 @@ class Redlock(object):
 
     def unlock(self, lock):
         for server in self.servers:
+            self.unlock_instance(server, lock.resource, lock.key)
+
+
+class ExpiryExtendingRedlock(Redlock):
+
+    def __init__(self, connection_list, **kwargs):
+        super(ExpiryExtendingRedlock, self).__init__(connection_list, **kwargs)
+        self.event = threading.Event()
+        self.expiry_extender = None
+
+    def extend_expiry_loop(self, server, resource, val, ttl):
+        while not self.event.is_set():
+            self.event.wait((ttl / 1000) - 5)
+            try:
+                if not server.set(resource, val, xx=True, px=ttl):
+                    # We couldn't extend the expiry, likely because resource doesn't exist.
+                    # Break out of this loop and allow resource to expire.
+                    self.event.set()
+            except:
+                self.event.set()
+
+    def stop_expiry_extender(self):
+        try:
+            self.event.set()
+            if self.expiry_extender is not None and self.expiry_extender.is_alive():
+                self.expiry_extender.join()
+            self.event.clear()
+        except:
+            pass
+
+    def start_expiry_extender(self, server, resource, val, ttl):
+        self.expiry_extender = threading.Thread(target=self.extend_expiry_loop, args=(server, resource, val, ttl))
+        self.expiry_extender.start()
+
+    def lock(self, resource, ttl):
+        retry = 0
+        val = self.get_unique_id()
+
+        # Add 2 milliseconds to the drift to account for Redis expires
+        # precision, which is 1 millisecond, plus 1 millisecond min
+        # drift for small TTLs.
+        drift = int(ttl * self.clock_drift_factor) + 2
+
+        while retry < self.retry_count:
+            n = 0
+            start_time = int(time.time() * 1000)
+            for server in self.servers:
+                if self.lock_instance(server, resource, val, ttl):
+                    n += 1
+                    self.start_expiry_extender(server, resource, val, ttl)
+            elapsed_time = int(time.time() * 1000) - start_time
+            validity = int(ttl - elapsed_time - drift)
+            if validity > 0 and n >= self.quorum:
+                return Lock(validity, resource, val)
+            else:
+                for server in self.servers:
+                    self.unlock_instance(server, resource, val)
+                retry += 1
+                time.sleep(self.retry_delay)
+        return False
+
+    def unlock(self, lock):
+        for server in self.servers:
+            self.stop_expiry_extender()
             self.unlock_instance(server, lock.resource, lock.key)
